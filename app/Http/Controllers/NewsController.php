6bb42ab55e65e923573f53f80c61ddb57d11eb24
raka aditya <adm@rakaaditya.com>
2015-12-21 19:17:08 +0700

    Move twitter method to twitter class

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -4,96 +4,25 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller as Base;
 use App\Http\Controllers\TwitterController as Twitter;
 use Illuminate\Http\Request;
-use GuzzleHttp\Client;
-use Cache;
 
 class NewsController extends Base
 {
-    const KEY = 'newstweet:news:';
 
-    function __construct()
-    {
-        $this->twitter = new Twitter();
-    }
+    public function index(Request $request, $screenName = 'liputan6dotcom')
+    {   
+        $articles = Twitter::screenName($screenName)
+                        ->count($request->input('count'))
+                        ->sinceId($request->input('since_id'))
+                        ->maxId($request->input('max_id'))
+                        ->get();
 
-    public function index(Request $request, $screen_name = 'liputan6dotcom')
-    {
-        $query = [
-            'screen_name'       => $screen_name,
-            'count'             => $request->input('count') ? $request->input('count') : 5,
-            'since_id'          => $request->input('since_id') ? $request->input('since_id') : null,
-            'max_id'            => $request->input('max_id') ? $request->input('max_id') : null,
-            'exclude_replies'   => true,
-            'include_rts'       => false
+        $result = [
+            'screen_name'   => $screenName,
+            'count'         => $request->input('count') ? $request->input('count') : 5,
+            'data'          => $articles
         ];
-
-        $key = self::KEY.$query['screen_name'].':'.$query['count'].':'.$query['since_id'].':'.$query['max_id'];
-
-        if(! $result = Cache::get($key) ) {
-            $data = $this->twitter->client()->get('statuses/user_timeline.json', [
-                'query' => $query
-            ])->getBody();
-
-            $data = json_decode($data);
-
-            $articles = [];
-
-            foreach($data as $row)
-                if($row->entities->urls[0]->expanded_url)
-                    $articles[] = $this->ogParse($row);
-
-            $result = [
-                'screen_name'   => $screen_name,
-                'count'         => $request->input('count') ? $request->input('count') : 5,
-                'data'          => $articles
-            ];
-            Cache::put($key, $result, 10);
-        }
 
         return response()->json($result)
                  ->setCallback($request->input('callback'));
-    }
-
-    private function ogParse($data)
-    {
-        libxml_use_internal_errors(true);
-
-        $url = $data->entities->urls[0]->expanded_url;
-
-        // In case URL using short URL
-        $url = $this->shortUrlParse($url);
-
-        $doc                = new \DOMDocument();
-        $doc->loadHTMLFile($url);
-        $xpath              = new \DOMXpath($doc);
-
-        $ogImageTag         = $xpath->query("*/meta[@property='og:image']")->item(0);
-        $ogTitleTag         = $xpath->query("*/meta[@property='og:title']")->item(0);
-        $ogDescriptionTag   = $xpath->query("*/meta[@property='og:description']")->item(0);
-        
-        $result = [
-            'tweet_id'      => $data->id,
-            'title'         => $ogTitleTag->attributes->getNamedItem('content')->nodeValue,
-            'image'         => $ogImageTag->attributes->getNamedItem('content')->nodeValue,
-            'description'   => $ogDescriptionTag->attributes->getNamedItem('content')->nodeValue,
-            'url'           => $url,
-            'retweet'       => $data->retweet_count,
-            'favorite'      => $data->favorite_count,
-
-        ];
-
-        return $result;
-    }
-
-    private function shortUrlParse($url)
-    {
-        $client = new Client(['base_uri' => $url]);
-
-        $response = $client->get('/');
-
-        if ($response->hasHeader('Location'))
-            $url = $response->getHeader('Location');
-
-        return $url;
     }
 }
--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -6,10 +6,20 @@ use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Subscriber\Oauth\Oauth1;
+use Cache;
 
 class TwitterController extends Base
 {
-    public function client()
+    const KEY = 'newstweet:news:';
+
+    private static  $screenName = 'cnnindonesia',
+                    $count = 5,
+                    $sinceId = null,
+                    $maxId = null,
+                    $excludeReplies = true,
+                    $includeRts = false;
+
+    private static function client()
     {
         $stack = HandlerStack::create();
 
@@ -29,5 +39,112 @@ class TwitterController extends Base
         ]);
 
         return $twitterClient;
+    }
+
+    public static function screenName($screenName)
+    {
+        if($screenName)
+            self::$screenName = $screenName;
+
+        return new self;
+    }
+
+    public static function count($count)
+    {
+        if($count)
+            self::$count = $count;
+
+        return new self;
+    }
+
+    public static function sinceId($sinceId)
+    {
+        if($sinceId)
+            self::$sinceId = $sinceId;
+
+        return new self;
+    }
+
+    public static function maxId($maxId)
+    {
+        if($maxId)
+            self::$maxId = $maxId;
+
+        return new self;
+    }
+
+    public static function get()
+    {
+        $query = [
+            'screen_name'       => self::$screenName,
+            'count'             => self::$count,
+            'since_id'          => self::$sinceId,
+            'max_id'            => self::$maxId,
+            'exclude_replies'   => true,
+            'include_rts'       => false
+        ];
+
+        $key = self::KEY.$query['screen_name'].':'.$query['count'].':'.$query['since_id'].':'.$query['max_id'];
+
+        if(! $articles = Cache::get($key) ) {
+            $data = self::client()->get('statuses/user_timeline.json', [
+                    'query' => $query
+            ])->getBody();
+
+            $data = json_decode($data);
+
+            $articles = [];
+
+            foreach($data as $row)
+                if($row->entities->urls[0]->expanded_url)
+                    $articles[] = self::ogParse($row);
+
+            Cache::put($key, $articles, 10);
+        }
+
+        return $articles;
+    }
+
+    private static function ogParse($data)
+    {
+        libxml_use_internal_errors(true);
+
+        $url = $data->entities->urls[0]->expanded_url;
+
+        // In case URL using short URL
+        $url = self::shortUrlParse($url);
+
+        $doc                = new \DOMDocument();
+        $doc->loadHTMLFile($url);
+        $xpath              = new \DOMXpath($doc);
+
+        $ogImageTag         = $xpath->query("*/meta[@property='og:image']")->item(0);
+        $ogTitleTag         = $xpath->query("*/meta[@property='og:title']")->item(0);
+        $ogDescriptionTag   = $xpath->query("*/meta[@property='og:description']")->item(0);
+        
+        $result = [
+            'tweet_id'      => $data->id,
+            'title'         => $ogTitleTag->attributes->getNamedItem('content')->nodeValue,
+            'image'         => $ogImageTag->attributes->getNamedItem('content')->nodeValue,
+            'description'   => $ogDescriptionTag->attributes->getNamedItem('content')->nodeValue,
+            'url'           => $url,
+            'retweet'       => $data->retweet_count,
+            'favorite'      => $data->favorite_count,
+
+        ];
+
+        return $result;
+    }
+
+    private static function shortUrlParse($url)
+    {
+        $client = new Client(['base_uri' => $url]);
+
+        $response = $client->get('/');
+
+        if ($response->hasHeader('Location'))
+            $url = $response->getHeader('Location');
+
+        return $url;
     }
 }
